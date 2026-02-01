3f4ddc934f4572eea9e17eae3d33b78d9c3c6b38
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-02-01 16:54:40 -0600

    Update src/utils/functionUtils.js

--- src/utils/functionUtils.js
+++ src/utils/functionUtils.js
@@ -1,29 +1,64 @@
 
 export function validateUserInfo(username, email, password) {
-   let name_trimmed = username.trim();
+   let fullname_trimmed = username.trim();
    let email_trimmed = email.trim();
    let password_trimmed = password.trim();
 
-   const name_pattern = /^([a-zA-Z]{2,}\s[a-zA-z]{1,}'?-?[a-zA-Z]{1,}\s?([a-zA-Z]{1,})?)(,? (?:[JS]r\.?|II|III|IV))?$/g;
+
+   const fullname_pattern = /^([a-zA-Z-]{2,}\s[a-zA-z]{1,}'?-?[a-zA-Z]{1,}\s?([a-zA-Z]{1,})?)(,? (?:[JS]r\.?|II|III|IV))?$/g;
    const email_pattern = /^[!A-Z0-9#$&?*^~_%+-]+(\.[A-Z0-9!_%+-^]+)*?@[A-Z0-9-]+([A-Z0-9.-])*\.[A-Z]{2,}$/i;
 
-   if (name_trimmed.length === 0) {
-      return {isValid: false, error: 'Your first and last name are required!'};
+   const password_pattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[-+_!@#$%^&*?]).{8,32}$/i;
+   const lowercase_pattern = /^(?=.*[a-z])/g;
+   const uppercase_pattern = /^(?=.*[A-Z])/g;
+   const digit_pattern = /^(?=.*\d{1,})/g;
+   const special_pattern = /(?=.*[-+_!@#$%^&*?])/g;
+
+
+   /************************* fullname conditional statements *************************/
+
+   if (fullname_trimmed.length === 0) {
+      return {isValid: false, error: 'First and last name are required!'};
+
+   } else {
+      if (!fullname_trimmed.match(fullname_pattern)) {
+         return {isValid: false, error: 'Enter a valid first and last name!'};
+      }
    }
-   if (!name_trimmed.match(name_pattern)) {
-      return {isValid: false, error: 'Enter your first and last name!'};
-   }
+
+   /************************* email conditional statements *************************/
+
    if (email_trimmed.length === 0) {
-      return {isValid: false, error: 'Your email is required!'};
+      return {isValid: false, error: 'Email is required!'};
+
+   } else {
+      if (!email_trimmed.match(email_pattern)) {
+         return {isValid: false, error: 'Enter a valid email!'};
+      }
    }
-   if (!email_trimmed.match(email_pattern)) {
-      return {isValid: false, error: 'Enter a valid email!'};
-   }
+
+   /************************* password conditional statements *************************/
+
    if (password_trimmed.length === 0) {
-      return {isValid: false, error: 'Your password is required!'};
+      return {isValid: false, error: 'Password is required!'};
+
+   } else if (!password_trimmed.match(lowercase_pattern)) {
+      return {isValid: false, error: 'Password must contain at least one lowercase letter!'};
+
+   } else if (!password_trimmed.match(uppercase_pattern)) {
+      return {isValid: false, error: 'Password must contain at least one uppercase letter!'};
+
+   } else if (!password_trimmed.match(digit_pattern)) {
+      return {isValid: false, error: 'Password must contain at least one number!'};
+
+   } else if (!password_trimmed.match(special_pattern)) {
+      return {isValid: false, error: `Password must include at least one: '-+_!@#$%^&*?'!`};
+
+   } else if (!password_trimmed.match(password_pattern)) {
+      return {isValid: false, error: 'Password must be between 8 and 32 characters long!'};
+
+   } else {
+      return {isValid: true};
    }
-   if (password_trimmed.length < 8) {
-      return {isValid: false, error: 'Password must be at least 8 characters!'};
-   }
-   return {isValid: true};
+
 }
